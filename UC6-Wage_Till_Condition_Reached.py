561c831372592c57c417f27a23067ecab55bd14a
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2023-01-14 22:10:26 +0530

    [Shubham]Added. Calculating monthly wage of an Employee till condition for maximum Hours and days is reached

---
+++ UC6-Wage_Till_Condition_Reached.py
@@ -0,0 +1,37 @@
+import random
+
+
+def calculate_monthlywage():
+    """
+       This function Calculates Monthly wage of an Employee
+    :return: None
+    """
+    is_full_time = 0
+    is_part_time = 1
+    full_time_hour = 8
+    part_time_hour = 4
+    wage_per_hour = 20
+    maximum_working_days = 20
+    maximum_working_hours = 100
+    total_working_hour = 0
+    days = 0
+    while days < maximum_working_days and total_working_hour < maximum_working_hours:
+        employee_status = random.randint(0, 2)
+        if employee_status == is_full_time:
+            total_working_hour += full_time_hour
+            print("Employee is Present for Full time")
+        elif employee_status == is_part_time:
+            total_working_hour += part_time_hour
+            print("Employee is Present for Part time")
+        else:
+            total_working_hour += 0
+            print("Employee is Absent")
+        days += 1
+    monthly_wage = total_working_hour * wage_per_hour
+    print("Monthly Wage of an Employee is:", monthly_wage)
+    print("Total Hours Worked:", total_working_hour)
+    print("Total Days Worked:", days)
+
+
+if __name__ == '__main__':
+    calculate_monthlywage()
